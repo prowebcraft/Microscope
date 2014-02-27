7b7fa6201e0430509197cf3b9784134be12f6b83
ProWebCraft <prowebcraft@gmail.com>
2014-02-27 13:35:42 +0400

    Initial commit. Working meteor project

---
+++ client/view/posts/post_edit.js
@@ -0,0 +1,26 @@
+Template.postEdit.events({
+   'submit form': function(e) {
+      e.preventDefault();
+      var currentPostId = this._id;
+      var postProperties = {
+         url: $(e.target).find('[name=url]').val(),
+         title: $(e.target).find('[name=title]').val()
+      }
+      Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+         // display the error to the user
+         alert(error.reason);
+      } else {
+         Router.go('postPage', {_id: currentPostId});
+      }
+      });
+   },
+   'click .delete': function(e) {
+      e.preventDefault();
+      if(confirm('Точно удалить данный пост?')) {
+         var currentPostId = this._id;
+         Posts.remove(currentPostId);
+         Router.go('postsList');
+      }
+   }
+});
